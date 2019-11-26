63f306ca94ba60dfb1ce9598074e67e46a5eb681
Luis Auz <luisauz.geek@gmail.com>
2019-11-25 22:22:31 -0500

    refactoring: JS coding

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".nav-icon").click(function(){
+        $(".full-nav").addClass("open");
+    });
+
+    $(".nav-close").click(function(){
+        $(".full-nav").remove("open");
+    });
+})
